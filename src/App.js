bafdfe685f31301e741db6a9c6b84fcd0aa4ae39
fehbarea <fehbarea@icloud.com>
2024-11-19 14:52:41 -0300

    componets header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+    <Header/>
       <h1>hellow world</h1>
       <p>Ola mundo 2</p>
+      <Footer/>
     </div>
+    
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css';
+
+function Footer(){
+    return(
+        <footer className={styles.footer} >
+            <h2>
+                GatoPretoTV &copy; Desenvolvido por Felipe Barea 2024
+            </h2>
+        </footer>
+    )
+};
+
+export default Footer;
