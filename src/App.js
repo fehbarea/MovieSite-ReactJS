f68b164bbae35cd5dadf99b076cb8622b2ffa2b9
fehbarea <fehbarea@icloud.com>
2024-11-19 15:57:18 -0300

    ajuste footer - cricao de componente container e banner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-    <Header/>
-      <h1>hellow world</h1>
-      <p>Ola mundo 2</p>
-      <Footer/>
+      <Header />
+      <Banner image={'home'} />
+      <Container>
+        <h1>hellow world</h1>
+        <p>Ola mundo 2</p>
+      </Container>
+      <Footer />
     </div>
-    
+
   );
 }
 
