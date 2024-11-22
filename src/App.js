df0cd0460a927eb70daf1acc6f0ce069685d5373
fehbarea <fehbarea@icloud.com>
2024-11-22 18:39:48 -0300

    componet card criado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Banner from "./components/Banner";
+import Card from "./components/Card";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import videos from "./json/db.json"
 
 function App() {
   return (
@@ -9,8 +11,16 @@ function App() {
       <Header />
       <Banner image={'home'} />
       <Container>
-        <h1>hellow world</h1>
-        <p>Ola mundo 2</p>
+
+      <h2>Geografia</h2>
+
+        <section className="cards">
+
+          {
+            videos.map(video => <Card id={video.id} key={video.id}/>)
+          }
+
+        </section>
       </Container>
       <Footer />
     </div>
