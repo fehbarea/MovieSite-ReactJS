36e07dec3d95e68196550481fe1ad50c35e33de0
fehbarea <fehbarea@icloud.com>
2024-12-19 21:27:22 -0300

    alteracao de componetes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,17 @@
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css'
 
 function Header(){
     return(
         <header className={styles.header}>
-            <span>GatoPretoTV</span>
+            <Link to="/">
+                <span>GatoPretoTV</span>
+            </Link>
             <nav>
-                <a href="" >Home</a>
-                <a href="">Assistir</a>
+
+                <Link to="/">Home</Link>
+                <Link to="/watch">Assistir</Link>
+
             </nav>
         </header>
     )
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Watch from "./Pages/Watch";
+
+
+function AppRoutes(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/watch" element={<Watch/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes;
